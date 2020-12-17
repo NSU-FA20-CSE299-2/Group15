9a9f6da3ce873a040f8670a61feb61e713da58c3
zsabit <sabit.zubair@northsouth.edu>
2020-12-17 20:54:10 +0600

    Learning #7: Added the template in views

--- Code/sabit_practice/django/test_project/test_app/views.py
+++ Code/sabit_practice/django/test_project/test_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'main.html')
 
 def contact(request):
     return HttpResponse("Test Contacts")
