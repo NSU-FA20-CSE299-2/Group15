b49825a4cf89baead1d4bfd7d2b5b15c9d2773da
zsabit <sabit.zubair@northsouth.edu>
2021-01-06 04:55:00 +0600

    Enhance #15: added country list to database added all coutry list in channels using django-countries 7.0 by adding it to the project

--- Code/IPTV_Solution/frontend/models.py
+++ Code/IPTV_Solution/frontend/models.py
@@ -1,6 +1,6 @@
 from os import name
 from django.db import models
-
+from django_countries.fields import CountryField
 # Create your models here.
 
 class Customer(models.Model):
@@ -16,7 +16,7 @@ class Customer(models.Model):
 class Channel(models.Model):
     name = models.CharField(max_length=200, null=True)
     link = models.CharField(max_length=200, null=True)
-    country = models.CharField(max_length=200, null=True)
+    country = CountryField()
     description = models.CharField(max_length=400, null=True)
     date_created = models.DateTimeField( auto_now_add=True, null=True)
 
