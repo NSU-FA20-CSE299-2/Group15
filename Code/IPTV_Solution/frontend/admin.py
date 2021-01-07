a11df62cf2e4da9edb7cb9d6c1985b99294c594e
zsabit <sabit.zubair@northsouth.edu>
2021-01-07 16:09:55 +0600

    Enhance #18: Added category in admin.py

--- Code/IPTV_Solution/frontend/admin.py
+++ Code/IPTV_Solution/frontend/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Channel, Customer
+from .models import Category, Channel, Customer
 
 admin.site.register(Customer)
+admin.site.register(Category)
 admin.site.register(Channel)
 
