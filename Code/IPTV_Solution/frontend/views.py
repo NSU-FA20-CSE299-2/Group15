1b7b8a79625f4f0e51b827b9e4f7e0b8d8f02bcb
zsabit <sabit.zubair@northsouth.edu>
2021-01-08 18:07:08 +0600

    Enhance #20: get channel list from models to views We are quering channel list from model and passing it to http response

--- Code/IPTV_Solution/frontend/views.py
+++ Code/IPTV_Solution/frontend/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
     return render(request, 'home.html')
 
 def iptv(request):
-    return render(request, 'iptv.html')    
+    # getting all channels from database
+    channels = Channel.objects.all()
+    context = {'channels': channels}
+
+    # rendering the site
+    return render(request, 'iptv.html', channels)
+    
