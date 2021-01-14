daaff6134176f3a64a1d8ab64ac88a50895cdf96
zsabit <sabit.zubair@northsouth.edu>
2021-01-15 03:33:44 +0600

    Enhance #31: added UserCreation to generate form user UserCreationForm from django.contrib.auth.forms and used it to get a registration form friom django autth forms

--- Code/IPTV_Solution/frontend/views.py
+++ Code/IPTV_Solution/frontend/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
     return render(request, 'home.html')
 
 def registerPage(request):
-    context = {}
+    form = UserCreationForm()
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 def iptv(request):
