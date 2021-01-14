bcccec9495067c5214e1595c95ad3a589ddaedcb
zsabit <sabit.zubair@northsouth.edu>
2021-01-15 04:25:22 +0600

    Enhance #31 added basic login page definision

--- Code/IPTV_Solution/frontend/views.py
+++ Code/IPTV_Solution/frontend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
@@ -21,6 +21,12 @@ def registerPage(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+
+def loginPage(request):
+
+    context = {}
+    return render(request, 'login.html', context)
 
 def iptv(request):
     # getting all channels from database
