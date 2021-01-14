1890088c65c3201b8a11cd86b98b4b052f9867dc
zsabit <sabit.zubair@northsouth.edu>
2021-01-15 04:36:32 +0600

    Enhance #31: after register it redirected to login

--- Code/IPTV_Solution/frontend/views.py
+++ Code/IPTV_Solution/frontend/views.py
@@ -18,6 +18,7 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
 
     context = {'form': form}
     return render(request, 'register.html', context)
