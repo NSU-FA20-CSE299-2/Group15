3c4d2b9a6ade977fbf70a8b888a32d46084b1482
zsabit <sabit.zubair@northsouth.edu>
2021-01-15 03:15:26 +0600

    Enhance #11: Added registerPage path in urls.py

--- Code/IPTV_Solution/frontend/urls.py
+++ Code/IPTV_Solution/frontend/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path("iptv/", views.iptv, name="iptv"),
+    path("/register", views.registerPage, name='register'),
 ]
