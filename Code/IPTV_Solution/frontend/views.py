76eb76288752306c584e8b8c8ea47fef769a74d9
zsabit <sabit.zubair@northsouth.edu>
2021-01-17 03:02:10 +0600

    Enhance #31: added code for successful registratn

--- Code/IPTV_Solution/frontend/views.py
+++ Code/IPTV_Solution/frontend/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages
 
 from .models import *
 from .forms import CreateUserForm
@@ -18,6 +19,9 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
             return redirect('login')
 
     context = {'form': form}
